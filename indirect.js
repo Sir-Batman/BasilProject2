c2850133baad7253e9b5d639909981dc903d1ab0
Sir-Batman <cl.yates.42@gmail.com>
2015-11-18 15:11:50 +0000

    A little commenting

--- indirect.js
+++ indirect.js
@@ -1,5 +1,6 @@
+//Load the data from the json file, and append it to the table element in the webpage
 $.getJSON('./indirect.json', function(data) {         
-    console.log(data);
+    //console.log(data);
 	for (var i in data){
 		$("#information").append("<tr><td><a href="+data[i].link+">"+data[i].link+"</a></td><td>"+data[i].description+"</td><td>"+data[i].phone+"</td></tr>");
 	}
