4cd17d82a488a96ac2d12e9267090400af39c7a7
Andrew Tolvstad <tolvstaa@oregonstate.edu>
2015-11-24 20:31:58 -0500

    Reformat indirect.js for readability

--- htdocs/indirect/indirect.js
+++ htdocs/indirect/indirect.js
@@ -2,6 +2,12 @@
 $.getJSON('./indirect.json', function(data) {         
     //console.log(data);
 	for (var i in data){
-		$("#information").append("<tr><td><a href="+data[i].link+">"+data[i].link+"</a></td><td>"+data[i].description+"</td><td>"+data[i].phone+"</td></tr>");
+		$("#information").append(
+			"<tr>"+"
+				"<td><a href="+data[i].link+">"+data[i].link+"</a></td>"+
+				"<td>"+data[i].description+"</td>"+
+				"<td>"+data[i].phone+"</td>"+
+			"</tr>"
+		);
 	}
 });
