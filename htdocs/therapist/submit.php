1aa07ee4085c545f1ce6c0516f1d32e3f64f5b7a
Inityx <CTGBandy@gmail.com>
2015-12-06 16:02:28 -0800

    Added input form, database and accessor

--- htdocs/therapist/submit.php
+++ htdocs/therapist/submit.php
@@ -1,12 +1,16 @@
 <pre>
 <?php
 	$jsobase = '/var/www/basil/htdocs/therapist/therapist.json';
+	$newtherapist = array(
+		"name" => $_POST['name'],
+		"url" => $_POST['url'],
+		"phone" => $_POST['phone'],
+	);
 	//file_put_contents(
-	echo
+	print_r(
 		$jsobase, json_encode(
-			json_decode(file_get_contents($jsobase))['foo'] = $bar
+			json_decode(file_get_contents($jsobase))[] = $newtherapist
 		)
-	//)
-	;
+	);
 ?>
 <pre>
