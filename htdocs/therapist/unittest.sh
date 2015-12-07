56a850dbd7ff80891a39fc246fe82ea15c190f10
Andrew Tolvstad <tolvstaa@oregonstate.edu>
2015-12-06 22:04:08 -0500

    Fixed errors in unit test

--- htdocs/therapist/unittest.sh
+++ htdocs/therapist/unittest.sh
@@ -4,9 +4,9 @@ name=$(pwgen -n1)
 url=$(pwgen -n1)
 phone=$(pwgen -n1)
 
-curl --data "name=$name&url=$url&phone=&phone" basil.andrewtolvstad.com/therapist/submit.php
+curl --data "name=$name&url=$url&phone=$phone" basil.andrewtolvstad.com/therapist/submit.php
 
-if [ -n "$(grep "{\"name\":\"$name\",\"url\":\"$url\",\"phone\":\"$phone\"}]" therapist.json)" ]; then
+if [ -z "$(grep "{\"name\":\"$name\",\"url\":\"$url\",\"phone\":\"$phone\"}]" therapist.json)" ]; then
     echo "Test Failed: failed to insert."
 else
     echo "Test Succeded."
